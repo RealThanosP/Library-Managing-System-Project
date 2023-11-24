ca8cbd8e1aa40683b666120375a61b51a111eb92
RealThanosP <panathanos1@gmail.com>
2023-11-24 15:40:08 +0200

    Added Features

--- class_test.py
+++ class_test.py
@@ -1,82 +1,119 @@
-class Book:
-    def __init__(self, isbn, title, author, summary = "Summary: ", stock=0):
-        self.title = str(title)
-        self.isbn = int(isbn)
-        self.author = author
-        self.summary = summary
-        self.stock = int(stock)
- 
-    def add_summary(self, summary):
-        self.summary = summary
+class User:
+    '''Creates a user that can interact with the library'''
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        credentials = [self.name, self.password]
 
-    def add_stock(self, new_pieces):
-        self.stock += new_pieces
+
+    def sign_in(self, name, password):
+        if name == self.name and password == self.password:
+            #Open another window with access to the library
+            pass
+   
+    def loan_out(self, book):
+        return #To do
+
+
+    def return_in(self, book):
+        return #To do
+
+
+class Book:
+    '''Defines the books with their attributes'''
+    #Attributes of the book:
+    def __init__(self, isbn, title, author,stock=0):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.stock = stock
+
+
+    def __str__(self):
+        return f"({self.isbn}, {self.title}, {self.author}, {self.stock})"
+
+
+    def __repr__(self):
+        return f"({self.isbn}, {self.title}, {self.author}, {self.stock})"
+
 
 class Library:
-    def __init__(self, books):
-        self.books = {}
-
-    def book_loading(self):
-        #Loads a dictionary from the database.txt
-        database = open("LMS-Project/Code Ideas/database.txt", "r", encoding="utf-8")
-        for line in database:
-            line = line.strip().split("(|)")
-            #Adds new elements to the dictionary:
-            self.books = {**self.books, **{int(line[0]):line[1:]}}
-        database.close()
-
-    def is_in_Library(self, book):
-        #Atribute: book, must be a class:Book object
-        #Checks if a book object is in library
-        if book.isbn not in self.books:
-            return False
-        else:
-            return True
-        
-    def add_new_book(self):
-        try:
-        #Asks for inputs from the user
-            isbn = int(input("Enter isbn: "))
-            title = input("Enter book-title: ")
-            author = input("Enter the author: ")
-            summary = input("Enter a summary: ")
-            stock = int(input("Enter the pieces left: "))       
-        except ValueError:
-            print("The isbn or the total stock inputs are invalid, Try again: ")
-        
-        try:
-            atributes = [isbn, title, author, summary, stock]
-            book = ",".join(atributes)
-
-        #Adds the attributes of the book, seperated by commas 
-            database = open("LMS-Project/Code Ideas/database.txt", "a", encoding="utf-8")
-            database.write(book)
-            database.close()
-            
-        except UnboundLocalError:
-            return
-        
-class User:
-    admin_password = "library123"
-    def __init__(self, name="", admin_access=False):
-        self.name = name
-        self.admin_access = admin_access
-    
-    def is_admin(self, name, password):
-        name = input("Enter a name: ")
-        password = input("Enter password: ")
-        if name == "admin":
-            if password == self.admin_password:
-                self.name = name
-                self.admin_access = True
-        else:
-            self.name = name
+    def __init__(self, books):
+        self.books = []
 
 
+    def __str__(self):
+        return f"{self.books}"
+
+
+    def __repr__(self):
+        return f"{self.books}"
+
+
+    def start(self):
+        #Loads up the new library from a database.txt to a list self.books
+        with open("database.txt", "r", encoding="utf-8") as database:
+            for line in database:
+                line = line.strip().split("\t")
+                if line == [""]: continue
+            #Defines the attributes of the book on the line
+                isbn = line[0]
+                title = line[1]
+                author = line[2]
+                stock = line[3]
+                book = Book(isbn, title, author, stock)
+                self.books.append(book)
+        return self.books
+
+
+    def add_book(self, book):
+        #Adds a book to the database.txt and the list books of the library
+        if isinstance(book, Book):
+            with open("database.txt", "a", encoding="utf-8") as database:
+                attributes = [str(book.isbn), book.title, book.author, str(book.stock)]
+                book_str = "\t".join(attributes) + "\n"#The book attributes in a line seperated by tab spaces
+           
+            #Adds a new book only when it's not in the database
+                if str(book) in str(self.books): pass
+                else: database.write(book_str)
+        else:
+            print("That's not a valid book object")
+   
+    def remove_book(self):
+        return #To do??? IDK if it's going to be useful.I mean a good library is a big library right?
+
+
+    def add_stock(self, book):
+        return #To do
+
+
+    def sub_stock(self, book):
+        return #To do
+   
+    def find(self, book):
+        return #To do
+   
+
+
+def book():
+    user_input = input("Enter book attributes seperated by tabs(isbn, title, author, stock): ")
+    user_input = user_input.strip(" ").split("\t")
+    book = Book(user_input[0], user_input[1], user_input[2], user_input[3])
+    return book
 
 
 
-    
+
+def main():
+    library = Library([])
+    library.start()
+    library.add_book(book())
+    print(library)
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 
