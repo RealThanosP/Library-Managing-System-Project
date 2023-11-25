69b7a7a967535ff1b659cb95fa5d847256307ed5
RealThanosP <panathanos1@gmail.com>
2023-11-25 15:00:27 +0200

    Added Features

--- gui.py
+++ gui.py
@@ -1,11 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-class DatabaseApp:
-    def __init__(self, root):
-        self.root = root
 
 class AddBookPopApp:
-    add_book_entries = []
+    editBookEntries = []
     add_placeholder = ("Isbn:", "Title:", "Author:", "Stock:")
 
     def __init__(self, root):
@@ -27,13 +24,13 @@ class AddBookPopApp:
     #Entries for the book data inputs
         for i in range(4):
         #Creates 5 input fielsds to fill:
-            self.input_book = tk.Entry(self.frame, borderwidth=5, width=25)
-            self.input_book.grid(row=i+1, column=1, sticky="w")
+            self.editBookField = tk.Entry(self.frame, borderwidth=5, width=25)
+            self.editBookField.grid(row=i+1, column=1, sticky="w")
         #Adds all the entries to a list to be used later to extract the data:
-            self.add_book_entries.append(self.input_book)
+            self.editBookEntries.append(self.editBookField)
         #Create the pointing labels to the entries:
-            self.add_book_label = tk.Label(self.frame, text=self.add_placeholder[i], justify="right")
-            self.add_book_label.grid(row=i+1, column=0, sticky="we")
+            self.editBookLabel = tk.Label(self.frame, text=self.add_placeholder[i], justify="right")
+            self.editBookLabel.grid(row=i+1, column=0, sticky="we")
 
     #Add Button
         self.addButton = tk.Button(self.frame, 
@@ -52,8 +49,8 @@ class AddBookPopApp:
         book_data = ""
         #The for-loop that Gives access to the entries separetely
         for i in range(4):
-            book_data += self.add_book_entries[i].get() + "\n"
-            self.add_book_entries[i].delete(0,"end")
+            book_data += self.editBookEntries[i].get() + "\n"
+            self.editBookEntries[i].delete(0,"end")
         
         book_data = book_data.split("\n")
 
@@ -81,15 +78,15 @@ class AddBookPopApp:
             self.errorLabel.config(text="Fill all the required fields")
 
 class App:
-    add_book_entries = []
-    loan_book_entries = []
+    editBookEntries = []
+    loanBookEntries = []
 
-    add_placeholder = ("Isbn:", "Title:", "Author:", "Summary:", "Stock:")
+    edit_placeholder = ("Isbn:", "Title:", "Author:", "Summary:", "Stock:")
     loan_placeholder = ("Find by Isbn:", "Find by Title:")
     def __init__(self, root):
         self.root = root
         self.root.title("Library Management System")
-        self.root.geometry("900x900")
+        self.root.geometry("700x600")
     #Defines the main-frame and packs it into the screen:
         self.frame = tk.Frame(root)
         self.frame.pack()
@@ -105,7 +102,7 @@ class App:
 
     #Adding-book-section:
     #Add-a-book-button
-        self.editBook = tk.Button(self.frame, text="Add Book", command=self.editBookDetails)
+        self.editBook = tk.Button(self.frame, text="Edit Book", command=self.editBookDetails)
         self.editBook.grid(row=1, column=0, columnspan=2)
     #Error-text-label:
         self.errorLabel = tk.Label(self.frame)
@@ -113,33 +110,33 @@ class App:
     #The inputs for the atributes of the new book:
         for i in range(0,5):
         #Creates 5 input fielsds to fill:
-            self.input_book = tk.Entry(self.frame, borderwidth=5, width=25)
-            self.input_book.grid(row=i+2, column=1, sticky="w")
+            self.editBookField = tk.Entry(self.frame, borderwidth=5, width=25)
+            self.editBookField.grid(row=i+2, column=1, sticky="w")
         #Adds all the entries to a list to be used later to extract the data:
-            self.add_book_entries.append(self.input_book)
+            self.editBookEntries.append(self.editBookField)
         #Create the pointing labels to the entries:
-            self.add_book_label = tk.Label(self.frame, text=self.add_placeholder[i], justify="right", width= 2)
-            self.add_book_label.grid(row=i+2, column=0, sticky="we")
+            self.editBookLabel = tk.Label(self.frame, text=self.edit_placeholder[i], justify="right")
+            self.editBookLabel.grid(row=i+2, column=0, sticky="we")
 
     #Loaning-book-section:
     #Loan-book-button
         self.loanButton = tk.Button(self.frame, text="Loan")
         self.loanButton.grid(row=1, column=2,columnspan=2)
-    #Loan-labels:
+        #Loan-labels:
         for i in range(2,4):
-            self.loan_label = tk.Label(self.frame, text=self.loan_placeholder[i-2],width=4)
-            self.loan_label.grid(row=i, column=2, sticky="we")
+            self.loanLabel = tk.Label(self.frame, text=self.loan_placeholder[i-2])
+            self.loanLabel.grid(row=i, column=2, sticky="we")
     
     #Loan-book-field:
         for i in range(2,4):
-            self.loan_input = tk.Entry(self.frame, borderwidth=5, width=25)
-            self.loan_input.grid(row=i,column=3,sticky="w")
-            self.loan_book_entries.append(self.loan_input)
+            self.loanBookField = tk.Entry(self.frame, borderwidth=5, width=25)
+            self.loanBookField.grid(row=i,column=3,sticky="w")
+            self.loanBookEntries.append(self.loanBookField)
             
     #Return-book-section:
     #Return-button:
-        self.return_button = tk.Button(self.frame, text="Return")
-        self.return_button.grid(row=1, column=4)
+        self.returnButton = tk.Button(self.frame, text="Return")
+        self.returnButton.grid(row=1, column=4)
 
     #kill button (for develiping purposes only)
         self.kill = tk.Button(self.frame, text="KILL", font=("Ariel", 20), command=self.kill)
@@ -156,13 +153,13 @@ class App:
             for book in database:
                 book = book.split("\t")
                 #Checks if the Isbn entry input is already in the database.txt
-                if self.add_book_entries[0].get() == book[0]:
+                if self.editBookEntries[0].get() == book[0]:
                     found = True
 
                 if found: break#Prematurely terminates the for-loop to make the process kind of faster
 
                 #Checks if the Isbn entry input is empty
-                if not self.add_book_entries[0].get():
+                if not self.editBookEntries[0].get():
                     found = True
                     self.errorLabel.config(text="Fill all the required fields")
                     break
