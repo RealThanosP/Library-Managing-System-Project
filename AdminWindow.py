ffcd28da67b38b1dc1f88bb81aa318ab296c80e9
RealThanosP <panathanos1@gmail.com>
2023-12-22 15:31:30 +0200

    For the holidays

--- AdminWindow.py
+++ AdminWindow.py
@@ -1,39 +1,88 @@
 import tkinter as tk
-from tkinter import messagebox
-from PIL import Image,ImageTk
+from tkinter import ttk, messagebox
 from database import Database, Book
+from PIL import Image,ImageTk
 
-
-class AdminPopUp:
-
-    def __init__(self, root):
-        self.BookAttributes = ("Isbn:", "Title:", "Author:", "Section:", "Stock:")
+class BookBrowsing:
+    def __init__(self, root, username):
+        self.name = username
         self.root = root
-        self.root.geometry("900x500")
-        self.root.title("Admin Menu")
-        self.root.resizable(False, False)
+        self.root.minsize(1000,700)
+        self.root.geometry("850x700")
+        self.root.title(f"Our Library")
         
-        #Main frame
-        self.frame = tk.Frame(self.root, bg="grey")
-        self.frame.pack(expand=True, fill="both")
+        #Initialize the library
+        self.library = Database("Library.db")
+        
+        #Top frame:
+        self.frameTop = tk.Frame(self.root, bg="#D2B48C")
+        self.frameTop.pack(fill="both")
+
+        #Style of the table:
+        self.tableStyle = ttk.Style()
+        self.tableStyle.configure("Treeview", 
+            highlightthickness=0, 
+            bd=0, 
+            font=('Calibri', 11),
+            background= "#B0A090")
+        self.tableStyle.configure("Treeview.Heading", 
+            font=('Calibri', 13,'bold'))
+        self.tableStyle.layout("Treeview", [('Treeview.treearea', {'sticky': 'nswe'})])
+        self.tableStyle.map("Treeview", background=[("selected", "green")])
+
+        #Frame for the table tree
+        self.frameTable = tk.Frame(self.frameTop, bg="#D2B48C")
+        self.frameTable.pack()
+
+        #Table scrollbar:
+        self.tableScroll = tk.Scrollbar(self.frameTable)
+        self.tableScroll.pack(side="right", fill="y", pady=20)
+        
+
+        self.table = ttk.Treeview(self.frameTable, yscrollcommand=self.tableScroll.set)
+        self.tableScroll.configure(command=self.table.yview)
+        self.table.bind("<<TreeviewSelect>>", self.table_on_select)
+        #Define the columns of the table:
+        self.table["columns"] = ("ISBN", "Title", "Author", "Section", "Stock")
+        
+        #Format the columns:
+        self.table.column("#0", width=0, stretch="NO")
+        self.table.column("ISBN", width=100, minwidth=25)
+        self.table.column("Title", width=400, minwidth=125)
+        self.table.column("Author", width=180, minwidth=80)
+        self.table.column("Section", width=100, minwidth=60)
+        self.table.column("Stock", width=50, minwidth=25)
+
+        #Create the headings
+        self.table.heading("#0", text="", anchor="w")
+        self.table.heading("ISBN", text="ISBN", anchor="w", command=lambda: self.sort_column("ISBN", False))
+        self.table.heading("Title", text="Title", anchor="center", command=lambda: self.sort_column("Title", False))
+        self.table.heading("Author", text="Author", anchor="center", command=lambda: self.sort_column("Author", False))
+        self.table.heading("Section", text="Section", anchor="w", command=lambda: self.sort_column("Section", False))
+        self.table.heading("Stock", text="Stock", anchor="center", command=lambda: self.sort_column("Stock", False))
+        
+        #Fills up the table with database data
+        self.tableFill()
+        self.table.pack(pady=20, fill="y")
+
+        self.refreshButton = tk.Button(self.frameTop, text="⟳", command= self.refresh_table)
+        self.refreshButton.pack()
+        #Secondary frame to handle the buttons and the user interface
+        #of the changest to the table and the database
+        self.frame = tk.Frame(self.root, bg="black")
+        self.frame.pack(expand = True, fill="both")
 
         #Places the backround of the window
-        self.setBackround(self.frame, "background.png", 900, 500)
-        #Places the frame of the add book section on the screen
+        self.setBackround(self.frame, "background.png", 1300, 500)
+       
+
+        #Add book section
         self.addSection(self.frame)
-        self.frameAdd.place(relx=0.2, rely=0.05, anchor="n")
+        self.frameAdd.place(relx=0.2, rely=0.1, anchor="n")
 
-        #Places the frame of the edit book section on the screen
+        #Edit a book's details section
         self.editSection(self.frame)
-        self.frameEdit.place(relx=0.55, rely=0.05, anchor="n")
-
-        #Places the frame of the user management section
-        self.userSection(self.frame)
-        self.frameUser.place()
-
-        #Close button
-        self.closeButton = tk.Button(self.frame, text="Close", font=("Helvetica", 20), padx=15, pady=15, command=self.kill)
-        self.closeButton.place(relx=0.5, rely=0.99,anchor="s")
+        self.frameEdit.place(relx=0.5, rely=0.1, anchor="n")
     
     def setBackround(self, frame, image, width, height):
         #Sets up the backroound
@@ -45,9 +94,49 @@ class AdminPopUp:
         self.backround.create_image(0, 0, image=self.bg, anchor="nw")
         self.backround.pack(fill="both", expand=True)
 
+    def findBook(self):
+        '''Main search function. Returns tuple: (isFound, string of the book found)'''
+        isFound = False
+        #Gets the inputs form the entries
+        isbn = self.editBookEntries[0].get()
+        title = self.editBookEntries[1].get()
+        author = self.editBookEntries[2].get()
+        error = ""#Controls the text shown to the user based on if book is found
+        
+        library = Database("Library.db")
+        book = library.get_book(isbn, title, author)
+        
+        if book: isFound = True
+
+        return isFound, book
+
+    def table_on_select(self, event):
+        try:
+            selectedBook = self.table.selection()[0]  # Get the ID of the selected book
+            bookValues = self.table.item(selectedBook, "values") #Extracts it's value
+        except Exception as error:
+            print(str(error))
+            return
+
+        try:
+            for index, entry in enumerate(self.editBookEntries):
+                entry.delete(0,"end")
+                entry.insert(0, bookValues[index])
+        except Exception as error:
+            for index, entry in enumerate(self.editBookEntries):
+                entry.delete(0,"end")
+
+    def tableFill(self):
+        book_list = self.library.show_database()
+
+        for book in book_list:
+            #Inserts the values
+            self.table.insert("", "end", value=book)
+
     #Add Book Frame
     def addSection(self, frame):
         #Main frame:
+        self.BookAttributes = ("Isbn:", "Title:", "Author:", "Section:", "Stock:")
         self.addBookEntries = []
         self.frameAdd = tk.Frame(frame, bg="#D2B48C")
 
@@ -84,14 +173,14 @@ class AdminPopUp:
                                     command=self.addBook)
 
         #Error Label at the bottom 
-        self.errorLabelAdd = tk.Label(self.frameAdd, text="", bg="#D2B48C")
+        self.errorLabelAdd = tk.Label(self.frameAdd, text="", bg="#D2B48C", justify="center")
 
         #Places evry frame on the screen:
         self.topLabel.grid(row=0, column=1, columnspan=2, padx=(0,70), pady=10)
         self.frameOfLabels.grid(row=1, column=0)
         self.frameOfEntries.grid(row=1, column=1)
         self.addButton.grid(row=2, column=0, columnspan=2, pady=(20,0))
-        self.errorLabelAdd.grid(row=3, column=0, columnspan=2, pady=(6,0))
+        self.errorLabelAdd.grid(row=3, column=0, columnspan=3, pady=(10,0))
 
     def addBook(self):
         '''Function for the SUMBIT button'''
@@ -116,7 +205,7 @@ class AdminPopUp:
             check = database.add_book_database(isbn, title, author, section, stock)
 
             #Let the user know the status of the bookAdding process
-            if check == "This book is already in the database.\n You can edit it's details on the other window.":
+            if check == "This book is already in the database.\nYou can edit it's details on the other window.":
                 self.errorLabelAdd.config(text=check)
             elif check == 'Book successfully added!':
                 messagebox.showinfo(title="Adding Books", message="Book Added ✔")
@@ -126,7 +215,7 @@ class AdminPopUp:
 
         #Just makes sure isbn and the remaining items are integers:
         except ValueError:
-            self.errorLabelAdd.config(text="The data you entered are invalid\n Try again: ")
+            self.errorLabelAdd.config(text="The data you entered are invalid, Try again: ")
             
     #Edit Book Frame
     def editSection(self, frame):
@@ -170,21 +259,21 @@ class AdminPopUp:
             self.editBookLabel = tk.Label(self.frameEditLabels, text=self.BookAttributes[i], justify="center", bg="#D2B48C")
             self.editBookLabel.pack(pady=7)
 
-        #Find Button
-        self.findButton = tk.Button(self.frameEditButtons, text="Find", 
-                                    pady=17, padx=12,
-                                    font=("Helvetica", 20),
-                                    bg="#CFCFCF",
-                                    command=self.editBook)
-        self.findButton.pack(side="left")
-
         #Submit Button
-        self.submitButton = tk.Button(self.frameEditButtons, text="Submit\nChanges", 
-                                    pady=2, padx=10,
+        self.submitButton = tk.Button(self.frameEditButtons, text="Submit", 
+                                    pady=15, padx=15,
                                     font=("Helvetica", 20),
                                     bg="#CFCFCF",
                                     command=self.submitChanges)
-        self.submitButton.pack(side="right")
+        self.submitButton.pack(side="left")
+
+        #Submit Button
+        self.clearButton = tk.Button(self.frameEditButtons, text="Clear", 
+                                    pady=15, padx=15,
+                                    font=("Helvetica", 20),
+                                    bg="#CFCFCF",
+                                    command=lambda: self.clear_text(self.editBookEntries))
+        self.clearButton.pack(side="right")
 
         #Error-Label
         self.errorLabelEdit = tk.Label(self.frameEditError, text="", bg="#D2B48C")
@@ -195,7 +284,7 @@ class AdminPopUp:
         self.frameEditLabels.grid(row=1, column=0)
         self.frameEditEntries.grid(row=1, column=1)
         self.frameEditButtons.grid(row=2,column=0,columnspan=2, pady=(20,0))
-        self.frameEditError.grid(row=3,column=0,columnspan=2)
+        self.frameEditError.grid(row=3,column=0,columnspan=2, pady=(10,0))
     
     def editBook(self):
 
@@ -237,46 +326,36 @@ class AdminPopUp:
         library = Database("Library.db")
         error = library.updateInfo(isbn, title, author, section, stock)
         self.errorLabelEdit.config(text=error)
+        self.tableFill()
 
-    def findBook(self):
-        '''Main search function. Returns tuple: (isFound, string of the book found)'''
-        isFound = False
-        #Gets the inputs form the entries
-        isbn = self.editBookEntries[0].get()
-        title = self.editBookEntries[1].get()
-        author = self.editBookEntries[2].get()
-        error = ""#Controls the text shown to the user based on if book is found
-        
-        library = Database("Library.db")
-        book = library.get_book(isbn, title, author)
-        
-        if book: isFound = True
+    def sort_column(self, column, reverse):
+        if column in ("Stock", "ISBN", "Section"):
+            data = [(int(self.table.set(child, column)), child) for child in self.table.get_children("")]
+            data.sort(reverse=reverse)
+        else:
+            data = [(self.table.set(child, column), child) for child in self.table.get_children("")]
+            data.sort(reverse=reverse)
 
-        return isFound, book
+        for index, (val, child) in enumerate(data):
+            self.table.move(child, "", index)
+
+        self.table.heading(column, command=lambda: self.sort_column(column, not reverse))
     
-    def userSection(self, frame):
-        "Handles the frame of the user Management Section"
-        #Main frame
-        self.frameUser = tk.Frame(frame)
+    def clear_text(self, entryList):
+        '''Clears all the text from any set of entries you insert'''
+        for entry in entryList:
+            entry.delete(0,"end")
 
-        #Title frame
-        self.frameUserTitle = tk.Frame(self.frameUser)
-        self.userTitleLabel = tk.Label(self.frameUser, text="User \nManagement", font=("Helvetica", 18))
-        
-        #Menu to see all the users and edit their privileges
-
-        #Checkboxes for the admin privileges
-        
-        #Text with the book of it's user on the side
-
-        #Submit button
-
-        #Error label
-     
-    def kill(self):
-        self.root.destroy()
+        if entryList == self.editBookEntries:
+            self.errorLabelEdit.config(text="Text successfully cleared")
+    def refresh_table(self):
+        # Clear existing treeview content
+        for item in self.table.get_children():
+            self.table.delete(item)
+        # Repopulate treeview with updated data
+        self.tableFill()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = AdminPopUp(root)
+    root  = tk.Tk()
+    app = BookBrowsing(root, "admin")
     root.mainloop()
